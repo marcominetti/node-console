139e003024aa7d1ae37afdb7f4de39e968f918a2
Marco Minetti <marco.minetti@novetica.org>
2015-03-02 23:30:20 +0100

    Added test.js file.

---
+++ test.js
@@ -0,0 +1,7 @@
+var web_console = require('./');
+
+var frontend_port = 9090;
+var agent_port = 9999;
+var listen_address = '0.0.0.0';
+
+web_console.start(frontend_port,agent_port,listen_address); 
