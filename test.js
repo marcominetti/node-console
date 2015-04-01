9ebd65ae37f0ec9b4d23be91074573632c9d930a
marco.minetti <marco.minetti@novetica.org>
2015-04-01 15:56:27 +0200

    Updated test.js file.

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-var web_console = require('./');
-
-var frontend_port = 9090;
 var agent_port = 9958;
 var listen_address = '0.0.0.0';
-var ecmascript_version = 7;      // supports values: 5,6,7 (through babel.js)
 
 // loading ECMAScript 6/7 polyfill if required (you need to manually install it)
-if (ecmascript_version > 5) {
-  require('babel/polyfill');
-}
+require('babel/polyfill');
 
-web_console.start(frontend_port,agent_port,listen_address,ecmascript_version); 
+console_agent = require('./agent.js');
+console_agent.start(agent_port||9958, listen_address||'0.0.0.0');
